44eb4972484e8444a5aba8233cbc4aa203b2a3cc
catiecook <catiecook888@gmail.com>
2017-02-05 13:37:50 -0700

    got the words searching etc.

--- anagram-finder/app/controllers/anagram_controller.rb
+++ anagram-finder/app/controllers/anagram_controller.rb
@@ -1,17 +1,4 @@
+
 class AnagramController < ApplicationController
-
-  def new_anagram
-    # anagrams = Hash.new()
-    # # structure
-    # anagrams[:key_aka_user_word, word_from_dictionary]
-    # #to add another item to the hash key:
-    # anagrams['key'].store 'word_from_dictionary'
-    #
-
-    # File.open("../assets/files/dictionary.txt", 'r') do |file| #open file
-    #   file.readlines.each_with_index do |line, index|
-    #     puts line if index < 100
-    end
-      # read words in, set a key to be the alphebetically sorted user input word, then search dictionary values accordingly.
 
 end
