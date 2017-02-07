e13adbb444828d93f2f37bb81a3662fd7853836f
catiecook <catiecook888@gmail.com>
2017-02-07 14:59:44 -0700

    gifs working

--- barbershop/public/javascripts/main/main.controller.js
+++ barbershop/public/javascripts/main/main.controller.js
@@ -1,23 +1,102 @@
 angular.module('gif')
 
 .controller('MainController', function($scope, mainService) {
-    $scope.gif = [];
+    $scope.tempGif = "";
+    $scope.allGifs = [];
     $scope.start = true;
     $scope.end = false;
     $scope.first = false;
+    $scope.counter = 0;
+    $scope.points = 0;
+
+    $scope.conditionals = {
+      puppy: false,
+      boulder: false,
+      tree: false,
+      east: false,
+      west: false,
+      keep: false,
+      leave: false,
+      shiny: false,
+      snack: false,
+      free: false,
+      craigslist: false,
+      stay: false
+    };
 
     $scope.start = function(cont) {
-      answer = cont.toLowerCase()
+      let answer = cont.toLowerCase()
       if(answer === "yes") {
         $scope.start = false;
         $scope.first = true;
       }
       if(answer === "no") {
+        $scope.start = false;
         $scope.end = true;
       }
     };
 
     $scope.getGif = (term) => {
-      $scope.gif = mainService.getGif(term) //service returns gif
+      $scope.counter += 1;
+
+      term = term.toLowerCase();
+
+      switch(term) {
+        case "puppy":
+          $scope.conditionals.puppy = true;
+          break;
+        case "boulder":
+          $scope.conditionals.boulder = true;
+          term = "rock climb";
+          break;
+        case "tree":
+          $scope.conditionals.tree = true;
+          break;
+        case "east":
+          $scope.conditionals.east = true;
+          $scope.conditionals.boulder = false;
+          break;
+        case "west":
+          $scope.conditionals.west = true;
+          $scope.conditionals.boulder = false;
+          $scope.conditionals.east = false;
+
+          break;
+        case "eat":
+          $scope.conditionals.eat = true;
+          $scope.conditionals.boulder = false;
+          $scope.points += 5;
+          break;
+        case "keep":
+          $scope.conditionals.keep = true;
+          $scope.points += 10;
+          break;
+        case "leave":
+          $scope.conditionals.leave = true;
+          $scope.end = true;
+          term = "face palm";
+          break;
+        case "shiny":
+          $scope.conditionals.shiny = true;
+          $scope.points += 30;
+          break;
+        case "stay":
+          $scope.conditionals.stay = true;
+          $scope.conditionals.west = false;
+          $scope.conditionals.east = false;
+          $scope.points += 5;
+          break;
+      }
+
+      $scope.first = false;
+
+      mainService.getGif(term)
+      .then((data) => {
+        console.log(data.data[0]);
+        // $scope.tempGif = data.data[0];
+        // $scope.allGifs.push({
+        //   1: data.data[0].embed_url
+        // });
+      });;
     }
 });
--- barbershop/public/javascripts/main/main.service.js
+++ barbershop/public/javascripts/main/main.service.js
@@ -1,12 +1,13 @@
 angular.module('gif')
   .service('mainService', function($http) {
-
-    $scope.getGif = function(term) {
-      let $url = "https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=dc6zaTOxFJmzC";
-
-      $.get(url)
-      .then((data) => {
-        console.log(data);
-      });
+    console.log("in service");
+   function getGif(term) {
+      let url = "https://api.giphy.com/v1/gifs/search?q=" + term + "&api_key=dc6zaTOxFJmzC";
+      return $.get(url)
     }
+
+    return {
+      getGif: getGif
+    };
+
   });
