53fd2a3863134d2decf86dbba04ed5d439ef739f
catiecook <catiecook888@gmail.com>
2017-02-07 12:11:41 -0700

    adding barbershop.io challenge

---
+++ barbershop/public/javascripts/main/directive.js
@@ -0,0 +1,21 @@
+angular.module('gif')
+
+  .directive('hello', function () {
+    return {
+        restrict: 'E',
+        template: '<p>Hello from directive</p>'
+    };
+  })
+
+  .directive('hcChart', function () {
+    return {
+        restrict: 'E',
+        template: '<div></div>',
+        scope: {
+            options: '='
+        },
+        link: function (scope, element) {
+            Highcharts.chart(element[0], scope.options);
+        }
+    };
+  })
