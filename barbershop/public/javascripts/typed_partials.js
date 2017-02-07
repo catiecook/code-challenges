9de2b34f620c4d6591906644358abe16defd8bd4
catiecook <catiecook888@gmail.com>
2017-02-07 12:10:21 -0700

    whoops

--- barbershop/public/javascripts/typed_partials.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('#element').typeIt({
-       strings: ["This is a great string.", "And here we have another great string.."],
-       speed: 50,
-       autoStart: false
-  });
-});
